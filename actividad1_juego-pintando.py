e1cf215d2f9566b034f3d4e57c1643c83c1e8732
ErnestoIRS <73501395+ErnestoIRS@users.noreply.github.com>
2020-10-26 17:19:04 -0600

    Update actividad1_juego-pintando.py

--- actividad1_juego-pintando.py
+++ actividad1_juego-pintando.py
@@ -21,9 +21,14 @@ def square(start, end):
 
     end_fill()
 
-def circle(start, end):
+def circles(start, end):
     "Draw circle from start to end."
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+    circle(end.x-start.x)
+    end_fill()
 
 def rectangle(start, end):
     "Draw rectangle from start to end."
@@ -59,9 +64,10 @@ onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
+onkey(lambda: color('cyan'), 'C')
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
-onkey(lambda: store('shape', circle), 'c')
+onkey(lambda: store('shape', circles), 'c')
 onkey(lambda: store('shape', rectangle), 'r')
 onkey(lambda: store('shape', triangle), 't')
 done()
